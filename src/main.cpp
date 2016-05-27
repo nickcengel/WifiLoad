6255afa566f1593197410b774c1aef79badea60e
nickcengel <nickcengel@gmail.com>
2016-05-26 18:34:04 -0700

    Back to Huzzah Encoder and Button work

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,45 @@
 
+
 #include <NEncoder.h>
+#include <Ticker.h>
 #include <arduino.h>
 
 const uint8_t pinA = 12;
 const uint8_t pinB = 13;
-const uint8_t buttonPin = 13;
+const uint8_t buttonPin = 14;
 const uint16_t encoderRangeA = 4095;
-const uint16_t encoderRangeB = 3;
 NEncoder encoder(pinA, pinB, buttonPin);
-void rotationHandler() { encoder.rotateRoutine(); }
-void buttonHandler() { encoder.buttonRoutine(); }
+static uint16_t oldg = 0;
+void rotationHandler();
+void buttonHandler();
 
 void setup() {
+  Serial.begin(115200);
+
   encoder.init(encoderRangeA);
   attachInterrupt(pinA, rotationHandler, RISING);
   attachInterrupt(buttonPin, buttonHandler, CHANGE);
 }
 
 void loop() {
-  uint8_t button = encoder.button();
-  encoder.clearPosition();
-  encoder.setRange(encoderRangeB);
+  uint16_t g = encoder.position();
+  if (g != oldg)
+    Serial.println(g);
+  oldg = g;
+  uint8_t butt = encoder.button();
+  if (butt)
+    Serial.println(butt);
 }
+//
+void rotationHandler() { encoder.rotateRoutine(); }
+void buttonHandler() { encoder.buttonRoutine(); }
 
-void mainScreen() {}
-void Iscreen() {}
-void Vscreen() {}
-void Pscreen() {}
-void Lscreen() {}
-void Sscreen() {}
-
-uint8_t stepSize(uint16_t tick) {}
+//
+// void mainScreen() {}
+// void Iscreen() {}
+// void Vscreen() {}
+// void Pscreen() {}
+// void Lscreen() {}
+// void Sscreen() {}
+//
+// uint8_t stepSize(uint16_t tick) {}
